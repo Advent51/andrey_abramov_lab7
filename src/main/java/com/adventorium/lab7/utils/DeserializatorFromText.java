be0ead03f15ecd54a9ef0dda162376af4e277c78
Andrey Abramov <Advent160@gmail.com>
2016-05-29 16:28:44 +0400

    Deserialization upgrade

--- src/main/java/com/adventorium/lab7/utils/DeserializatorFromText.java
+++ src/main/java/com/adventorium/lab7/utils/DeserializatorFromText.java
@@ -25,38 +25,33 @@ public class DeserializatorFromText {
         albums = new HashSet<>();
         songs = new HashSet<>();
 
+        int AUTHOR_INTRO_LENGTH = "Author: ".length();
+        int ALBUM_INTRO_LENGTH = "\tAlbum: ".length();
+        int GENRE_INTRO_LENGTH = "\t\tGenre: ".length();
+        int SONG_INTRO_LENGTH = "\t\t\tSong: ".length();
+        int DURATION_INTRO_LENGTH = "\t\t\t\tDuration: ".length();
+
         try (BufferedReader in = new BufferedReader(new FileReader(fileIn))) {
             String readedLine = in.readLine();
             while (readedLine != null) {
                 if (readedLine.startsWith("Author: ")) {
-                    Author author = new Author(readedLine.substring(8));
+                    Author author = new Author(readedLine.substring(AUTHOR_INTRO_LENGTH));
                     readedLine = in.readLine();
                     while (readedLine != null && readedLine.startsWith("\tAlbum: ")) {
-                        Album album = new Album(readedLine.substring(8));
+                        Album album = new Album(readedLine.substring(ALBUM_INTRO_LENGTH));
                         albums.add(album);
                         readedLine = in.readLine();
                         if (readedLine != null && readedLine.startsWith("\t\tGenre: ")) {
-                            album.addGenre(readedLine.substring(9));
+                            album.addGenre(readedLine.substring(GENRE_INTRO_LENGTH));
                             readedLine = in.readLine();
-                            while (readedLine != null && readedLine.startsWith("\t\t\tSong: ")) {
-                                Song song = new Song(readedLine.substring(9));
-                                songs.add(song);
-                                album.addSong(song);
-                                readedLine = in.readLine();
-                                if (readedLine != null && readedLine.startsWith("\t\t\t\tDuration: ")) {
-                                    song.addDuration(Integer.parseInt(readedLine.substring(14)));
-                                    readedLine = in.readLine();
-                                } else {
-                                    readedLine = in.readLine();
-                                }
-                            }
-                        } else while (readedLine != null && readedLine.startsWith("\t\t\tSong: ")) {
-                            Song song = new Song(readedLine.substring(12));
+                        }
+                        while (readedLine != null && readedLine.startsWith("\t\t\tSong: ")) {
+                            Song song = new Song(readedLine.substring(SONG_INTRO_LENGTH));
                             songs.add(song);
                             album.addSong(song);
                             readedLine = in.readLine();
                             if (readedLine != null && readedLine.startsWith("\t\t\t\tDuration: ")) {
-                                song.addDuration(Integer.parseInt(readedLine.substring(14)));
+                                song.addDuration(Integer.parseInt(readedLine.substring(DURATION_INTRO_LENGTH)));
                                 readedLine = in.readLine();
                             } else {
                                 readedLine = in.readLine();
@@ -77,39 +72,6 @@ public class DeserializatorFromText {
                     readedLine = in.readLine();
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //Well, I've just forgot about it
-    public static void readWithTokenizer(File fileIn) {
-        try (BufferedReader in = new BufferedReader(new FileReader(fileIn))) {
-            StreamTokenizer st = new StreamTokenizer(in);
-            boolean eof = false;
-            do {
-                int token = st.nextToken();
-                switch (token) {
-                    case StreamTokenizer.TT_EOF:
-                        System.out.println("End of File encountered.");
-                        eof = true;
-                        break;
-                    case StreamTokenizer.TT_EOL:
-                        System.out.println("End of Line encountered.");
-                        break;
-                    case StreamTokenizer.TT_WORD:
-                        System.out.println("Word: " + st.sval);
-                        break;
-                    case StreamTokenizer.TT_NUMBER:
-                        System.out.println("Number: " + st.nval);
-                        break;
-                    default:
-                        System.out.println((char) token + " encountered.");
-                        if (token == '!') {
-                            eof = true;
-                        }
-                }
-            } while (!eof);
         } catch (IOException e) {
             e.printStackTrace();
         }
